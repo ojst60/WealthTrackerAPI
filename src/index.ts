47655781c6f59ea13e01201a117674964c0a447d
Julius Oyovwikigho <julius.oyovwikigho@garrison.green>
2024-06-02 08:09:18 +0100

    add a dotenv config file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import express, { Request, Response } from "express";
+import dotenv from 'dotenv';
+import path = require("path");
+
+// Load env vars
+const loadEnvVar = dotenv.config({ path: path.resolve(__dirname,'../config.env') });
+
+if (loadEnvVar.error) {
+  console.error("Failed to load environment variables from config.env", loadEnvVar.error);
+  process.exit(1);
+}
+
 
 const objRes = [
   { id: 1, name: "julius" },
   { id: 2, name: "Abel" },
 ];
 
+console.log(process.env);
+
 const app = express();
 const port = 5000;
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-  res.status(200);
-  res.send(objRes);
+  res.status(200).json(objRes);
 });
 
-app.get("/api/${number}", (req, res: Response) => {
-  console.log(req);
+// Parameterized route
+app.get("/api/:number", (req: Request, res: Response) => {
+  console.log(req.params);
   res.send("This is my second route");
 });
 
